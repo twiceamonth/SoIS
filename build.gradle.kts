8dbe0241f9af6154584dd7b92d9db5aec2c69990
Alex <alexander12110@gmail.com>
2024-09-19 13:59:40 +0700

    add Tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    implementation(kotlin("stdlib"))
 }
 
 tasks.test {
