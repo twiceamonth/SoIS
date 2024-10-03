d55ab8ba6b2f9493a5dd1a8db8d99030fdd333b0
Maxim Vasenin <m.vasenin@bk.ru>
2024-10-03 13:25:32 +0700

    - lcg next

--- src/main/kotlin/algs/LCG.kt
+++ src/main/kotlin/algs/LCG.kt
@@ -2,11 +2,23 @@ package org.example.algs
 
 import org.example.utils.BaseUtils
 import kotlin.math.pow
+import kotlin.time.times
 
 class LCG(
     private val utils: BaseUtils,
     private val sBlock: SBlockImpl,
 ) {
+
+    fun LCG_next(state_in: Int, coeffs_in: List<Int>): Int {
+        val a = coeffs_in[0]
+        val c = coeffs_in[1]
+        val m = coeffs_in[2]
+
+        val out = (a * state_in + c) % m
+
+        return out
+    }
+
     //bpr_in и spr_in не матрицы а просто массивы
     fun make_coeffs(bpr_in: List<Int>, spr_in: List<Int>, pow_in: Int) : List<Int> {
         val ss = spr_in.min()
