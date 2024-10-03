bf4843b6479ab7e8d59acb2d016cfd8f9109194b
Maxim Vasenin <m.vasenin@bk.ru>
2024-10-03 13:19:18 +0700

    - генерация коэффициентов

---
+++ src/main/kotlin/algs/LCG.kt
@@ -0,0 +1,30 @@
+package org.example.algs
+
+import org.example.utils.BaseUtils
+import kotlin.math.pow
+
+class LCG(
+    private val utils: BaseUtils,
+    private val sBlock: SBlockImpl,
+) {
+    //bpr_in и spr_in не матрицы а просто массивы
+    fun make_coeffs(bpr_in: List<Int>, spr_in: List<Int>, pow_in: Int) : List<Int> {
+        val ss = spr_in.min()
+        val bs = bpr_in.min()
+        val bb = bpr_in.max()
+        val sb = spr_in.max()
+
+        // немножко закостылил
+        val max = (2.0.pow(pow_in)).toInt() - 1
+        var tmp = bs * ss
+        val a = ss * bs * sb + 1
+        val c = bb
+
+        for (i in 0..pow_in) {
+            if(tmp*ss >= max) tmp *= ss
+        }
+
+        val m = tmp
+        if(a<m && c<m) return listOf(a, c, m) else throw Error("wrong_guess")
+    }
+}
