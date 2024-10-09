07be0eb37b01eb724fc85a345ee5078f3c7a205c
Maxim Vasenin <m.vasenin@bk.ru>
2024-10-09 23:42:04 +0700

    - hclcg

--- src/main/kotlin/algs/HCLCG.kt
+++ src/main/kotlin/algs/HCLCG.kt
@@ -3,25 +3,26 @@ package org.example.algs
 import org.example.utils.BaseUtils
 
 class HCLCG(
-    private val lcg: LCG,
+    private val lcg: List<LCG>,
     private val utils: BaseUtils,
-    private var state_in:List<Int>,
-    private var set_in: List<List<Int>>,
 ) {
+    private var state = listOf<Int>()
+    fun getState(): List<Int> { return state }
+
     fun HCLCG_next(): Int {
-        val first = lcg.LCG_next(state_in[0], set_in[0])
-        val second = lcg.LCG_next(state_in[1], set_in[1])
-        val control = lcg.LCG_next(state_in[2], set_in[2])
+        val first = lcg[0].LCG_next()
+        val second = lcg[1].LCG_next()
+        val control = lcg[2].LCG_next()
 
         val n = utils.count_unity_bits(control)
-
         var out: Int
+
         if(control % 2 == 0) {
             out = utils.compose_num(first, second, n)
         } else {
             out = utils.compose_num(second, first, n)
         }
-        state_in = listOf(first, second, control)
+        state = listOf(first, second, control)
 
         return out
     }
