2a08b217a94ba8c7bdba2a2ab46a3b97153f1a39
Maxim Vasenin <m.vasenin@bk.ru>
2024-10-10 01:26:28 +0700

    - hclcg

--- src/main/kotlin/algs/HCLCG.kt
+++ src/main/kotlin/algs/HCLCG.kt
@@ -2,11 +2,18 @@ package org.example.algs
 
 import org.example.utils.BaseUtils
 
-class HCLCG(
-    private val lcg: List<LCG>,
-    private val utils: BaseUtils,
-) {
+class HCLCG(seed: List<Int>, coeffs: List<List<Int>>) {
+    private val lcg = mutableListOf<LCG>()
     private var state = listOf<Int>()
+
+    private val utils = BaseUtils()
+
+    init {
+        for (i in 0..2) {
+            lcg += LCG(seed[i], coeffs[i])
+        }
+    }
+
     fun getState(): List<Int> { return state }
 
     fun HCLCG_next(): Int {
