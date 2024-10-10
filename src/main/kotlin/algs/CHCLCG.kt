cf6fab412ddba07641d15eb10fb56bad4b03f299
Maxim Vasenin <m.vasenin@bk.ru>
2024-10-10 13:43:37 +0700

    - CHCLCG

--- src/main/kotlin/algs/CHCLCG.kt
+++ src/main/kotlin/algs/CHCLCG.kt
@@ -18,11 +18,38 @@ class CHCLCG(
         }
     }*/
 
-    fun CHCLCG_next(): String {
+    /*fun CHCLCG_next(): String {
         var stream = ""
 
         for (i in 0..3) {
             val substr = seed.substring(i * 4, (i * 4) + 4)
+            val seedSegment = sBlock.make_seed(substr)
+            seeds.add(utils.seed2nums(seedSegment))
+        }
+
+
+        // такое говнище наверное, писал как понял что хотят
+        for (j in 0..3) {
+            var tmp: Long = 0
+            var sign = 1
+            for (i in 0..3) {
+                hclcg += HCLCG(seeds[i], coeffs)
+                var res:Int = hclcg[i].HCLCG_next()
+                tmp = (1048576 + sign * res + tmp) % 1048576
+                sign = -sign
+            }
+            stream += utils.num2block(tmp.toInt())
+        }
+
+        return stream
+    }*/
+
+    fun CHCLCG_next(): String {
+        var stream = ""
+
+        val seed_c = sBlock.check_seed(seed)
+        for (i in 0..3) {
+            val substr = seed_c.substring(i * 4, (i * 4) + 4)
             val seedSegment = sBlock.make_seed(substr)
             seeds.add(utils.seed2nums(seedSegment))
         }
